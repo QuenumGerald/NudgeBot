12eac341e532f82de87f3a850f3b11b6e74d4216
Gérald Quenum <geraldquenum9@gmail.com>
2026-04-08 14:41:56 +0200

    new

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Starting NudgeBot server..."
-
-while true; do
-    node server-express.js
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] Server stopped (exit code $?), restarting in 2 seconds..."
-    sleep 2
-done
